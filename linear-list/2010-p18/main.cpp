f5ff1804abcf45e979a35557d97dd02f8d0d4cd7
xavier <a@nxw.name>
2019-11-25 11:17:01 +0800

    feat: added traverse() for SeqListPointer in 2010-p18

--- linear-list/2010-p18/main.cpp
+++ linear-list/2010-p18/main.cpp
@@ -7,30 +7,38 @@
 #define MaxSize 50
 
 typedef struct {
-    int size;
+    int length;
     int *data;
-} Array;
+} SeqList, *SeqListPointer;
 
-Array init(int size) {
+SeqList init(int size) {
     if (size > MaxSize) {
         printf("size you input is greater than MaxSize.\n");
     }
-    Array a;
-    a.size = size;
+    SeqList a;
+    a.length = size;
     a.data = (int *)malloc(sizeof(int) * size);
     for (int i = 0; i < size; i++) a.data[i] = i;
     return a;
 }
 
-void traverse(Array arr) {
+void traverse(SeqList arr) {
     printf("traverse(): ");
-    for (int i=0; i<arr.size; i++) {
+    for (int i=0; i<arr.length; i++) {
         printf("%d  ", arr.data[i]);
     }
     printf("\n");
 }
 
-void reverse(Array &arr, int start, int end) {
+void traverse(SeqListPointer arr) {
+    printf("traverse(): ");
+    for (int i=0; i<arr->length; i++) {
+        printf("%d  ", arr->data[i]);
+    }
+    printf("\n");
+}
+
+void reverse(SeqList &arr, int start, int end) {
     for (int i = 0; (i + start) <= ((start + end) / 2); i++) {
         int temp = arr.data[start+i];
         arr.data[start+i] = arr.data[end-i];
@@ -39,21 +47,21 @@ void reverse(Array &arr, int start, int end) {
 }
 
 // my_answer() is correct!
-void my_answer(Array &arr, int p) {
-    if (p < 0 || p >= arr.size) {
+void my_answer(SeqList &arr, int p) {
+    if (p < 0 || p >= arr.length) {
         printf("p is an invalid value.\n");
         return;
     }
     // reverse all elements
-    reverse(arr, 0, arr.size - 1);
+    reverse(arr, 0, arr.length - 1);
     // reverse all elements between `0` and `arr.size - p - 1`
-    reverse(arr, 0, arr.size - p - 1);
+    reverse(arr, 0, arr.length - p - 1);
     // reverse all elements between `arr.size - p` and `arr.size - 1`
-    reverse(arr, arr.size - p, arr.size - 1);
+    reverse(arr, arr.length - p, arr.length - 1);
 }
 
 int main() {
-    Array arr = init(10);
+    SeqList arr = init(10);
     traverse(arr);
 
     my_answer(arr, 4);
